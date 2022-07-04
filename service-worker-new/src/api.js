6f74098e8b1ce69d7271b6896e83c7910d33660d
Cristiano S. Oliveira <cristianounix@gmail.com>
2022-07-04 00:05:09 -0300

    Rename folder

--- service-worker-new/src/api.js
+++
@@ -1,8 +0,0 @@
-export default async function callApi (url){
-  const response = await fetch(url);
-  const body = await response.json();
-
-  if (response.status !== 200) throw Error(body.message);
-
-  return body;
-};
